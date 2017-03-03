e3cc93ae94db90fccf11d630ae17c384fea2e714
michael-palad <happyfighter2015@gmail.com>
2017-03-03 11:31:06 +0000

    add error handling and styling

--- app/models/yt_playlist.rb
+++ app/models/yt_playlist.rb
@@ -1,5 +1,6 @@
 class YtPlaylist
   include ActiveModel::Model
+  include ActionView::Helpers::TextHelper
   
   attr_accessor :url
   
@@ -8,23 +9,40 @@ class YtPlaylist
   attr_reader :total_duration
   
   def process_playlist
-    Yt.configuration.api_key = Rails.application.secrets.youtube_api_key
-    query_hash = Rack::Utils.parse_query URI(self.url).query
-    if query_hash['list'].nil?
-      errors.add(:url, "Not a valid playlist url")
+    begin
+      Yt.configuration.api_key = Rails.application.secrets.youtube_api_key
+      query_hash = Rack::Utils.parse_query URI(self.url).query
+      if query_hash['list'].nil?
+        errors.add(:url, "Not a valid playlist url")
+        return false
+      end
+      
+      playlist = Yt::Playlist.new(id: query_hash['list'])
+      @title = playlist.title
+      total_duration = Time.at(0)
+      @videos = []
+      playlist.playlist_items.each do |item|
+        begin
+          video = Yt::Video.new(id: item.video_id)
+          @videos << { title: video.title, length: video.length }
+        rescue
+          next
+        end
+        total_duration += video.duration
+      end
+      
+      if total_duration.day > 1
+        @total_duration = "#{pluralize(total_duration.day - 1 , "day")} and "
+        @total_duration += total_duration.strftime("%H:%M:%S")
+      else
+        @total_duration = total_duration.strftime("%H:%M:%S")
+      end
+    rescue
+      errors.add(:url, "Error retrieving playlist data")
       return false
     end
     
-    playlist = Yt::Playlist.new(id: query_hash['list'])
-    @title = playlist.title
-    total_duration = Time.at(0)
-    @videos = []
-    playlist.playlist_items.each do |item|
-      video = Yt::Video.new(id: item.video_id)
-      @videos << { title: video.title, length: video.length }
-      total_duration += video.duration
-    end
-    @total_duration = total_duration.strftime("%H:%M:%S")
+    return true
   end
   
 end
